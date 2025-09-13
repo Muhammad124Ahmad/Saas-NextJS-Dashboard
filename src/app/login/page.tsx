d24465e04e2b6c6c7554affd0fe5e2d6ac584505
Muhammad124Ahmad <muhammad510ahmad@gmail.com>
2025-09-13 12:10:41 +0500

    Fix react/no-unescaped-entities error by escaping apostrophes in login and signup pages

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -5,7 +5,7 @@ export default function LoginPage() {
     <div className="flex flex-col items-center justify-center min-h-[60vh]">
       <AuthForm mode="login" />
       <p className="mt-4 text-sm text-gray-600">
-        Don't have an account?{' '}
+        Don&apos;t have an account?{' '}
         <a href="/signup" className="text-[#2563eb] underline hover:text-[#1746a2]">Sign up</a>
       </p>
     </div>
